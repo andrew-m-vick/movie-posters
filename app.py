94ceae83fcc0b208009b8270443cda2335962f2a
SirBajaBlast <andrew.vick@me.com>
2026-04-08 23:56:34 -0500

    update revamp

--- app.py
+++ app.py
@@ -1,35 +1,12 @@
-from flask import Flask, render_template, request
-import requests
 import os
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-api_key = os.environ.get("OMDB_API_KEY")
-
-def get_poster_url(movie_title):
-    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
-    response = requests.get(url)
-    data = response.json()
-
-    if 'Poster' in data and data['Poster'] != 'N/A':
-        poster_url = data['Poster']
-        modified = poster_url[:poster_url.rfind("._V1_")+5] + "poster.jpg"
-        return modified, data.get('Title', movie_title)
-    return None, None
-
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    poster_url = None
-    title = None
-    error = None
+    return send_file('index.html')
 
-    if request.method == "POST":
-        movie_title = request.form.get("movie_title")
-        poster_url, title = get_poster_url(movie_title)
-        if not poster_url:
-            error = f"No poster found for '{movie_title}'"
-
-    return render_template("index.html", poster_url=poster_url, title=title, error=error)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
